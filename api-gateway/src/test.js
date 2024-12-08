c4023882bcaf02d426fde2cfea02e8cae1bf977f
MykhayloB13M <mykle23.dev@gmail.com>
2024-12-09 00:17:08 +0100

    crear primera entrada de datos

--- api-gateway/src/test.js
+++ api-gateway/src/test.js
@@ -1,12 +1,17 @@
 // sendBulkMessages.js
 const { publishToQueue } = require("./rabbitmq");
 const config = require("./config");
-async function sendBulkMessages() {
+function sendBulkMessages() {
   const queue = "telegram.messages";
+  const queue2 = "whatsapp.messages";
 
   for (let i = 0; i < 100; i++) {
     const message = { to: `usuario_${i}`, content: `Mensaje número ${i}` };
-    await publishToQueue(config.RABBITMQ_URL, queue, message);
+    publishToQueue(config.RABBITMQ_URL, queue, message);
+  }
+  for (let i = 0; i < 100; i++) {
+    const message = { to: `usuario_${i}`, content: `Mensaje número ${i}` };
+    publishToQueue(config.RABBITMQ_URL, queue2, message);
   }
 
   console.log("Se han enviado 100 mensajes a la cola");
@@ -14,7 +19,7 @@ async function sendBulkMessages() {
 
 // Ejecutar la función si se llama directamente este archivo
 if (require.main === module) {
-  sendBulkMessages().catch(console.error);
+  sendBulkMessages()
 }
 
 sendBulkMessages();
